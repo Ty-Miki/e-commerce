836cfcf18b209b643dbe6144ca8d76146f9904ac
Ty-Miki <ethioadvance@gmail.com>
2024-11-05 11:32:07 +0300

    Added views to add or update & remove products from the cart

--- myshop/carts/views.py
+++ myshop/carts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from django.http import HttpRequest
+from shop.models import Product
+from .cart import Cart
+from .forms import CartAddProductForm
 
-# Create your views here.
+@require_POST
+def cart_add(request: HttpRequest, product_id: int):
+
+    cart = Cart(request=request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product,
+                 quantity=cd['quantity'],
+                 override_quantity=cd['override'])
+
+    return redirect('cart:cart_detail')
+    
+@require_POST
+def cart_remove(request: HttpRequest, product_id: int):
+
+    cart = Cart(request=request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product=product)
+    return redirect('cart:cart_detail')
