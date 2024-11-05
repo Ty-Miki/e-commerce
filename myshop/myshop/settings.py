45a172a1cc8e6f7aded14824010a5d6227ce36ec
Ty-Miki <ethioadvance@gmail.com>
2024-11-05 11:21:20 +0300

    Added a form to add products to carts

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
-    'cart.apps.CartConfig',
+    'carts.apps.CartsConfig',
 ]
 
 MIDDLEWARE = [
