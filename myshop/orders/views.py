86f02644c7448633fe6d8cd46c769466c7bbb8dd
Ty-Miki <ethioadvance@gmail.com>
2024-11-11 11:40:08 +0300

    Added the necessary forms, views, urls and templates to place orders

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .models import OrderItem
+from .forms import OrderCreateForm
+from carts.cart import Cart
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+def order_create(request: HttpRequest) -> HttpResponse:
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            # Clear the cart
+            cart.clear()
+
+
+        return render(request,
+                      'orders/order/created.html',
+                      {'order': order})
+    
+    else:
+        form = OrderCreateForm()
+    
+    return render(request,
+                  'orders/order/create.html',
+                  {'cart': cart, 'form': form})
+                      
+    
